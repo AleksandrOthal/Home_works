487b63a47655d81876486694fed0169b5b083d55
Alex <alex8graph6@gmail.com>
2019-12-02 23:19:41 +0200

    home_work

--- home_work_5.rb
+++ home_work_5.rb
@@ -1,13 +1,13 @@
 [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].each do |n|
-    p n
+  p n
 end
 
 puts "       task 2"
 
 [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].each do |n|
-    if n>5
-        p n
-    end
+  if n>5
+    p n
+  end
 end
 
 puts "       task 3"
@@ -18,4 +18,3 @@ n.push(11)
 n.each do |l|
   puts l
 end
-
